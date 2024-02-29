6229ec022b3801ff531c8c7b3cb00bd85844c7ad
Aleksander Romero <aleksandercgr@gmail.com>
2024-02-29 13:00:27 -0300

    ⚡️ Inclusão da sessão Homepage

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,12 @@
+// CONTROLE BACKGROUND ESTRELAS
+
 let interval;
 const video = document.getElementById("video-background");
 const checkbox = document.getElementById("checkbox");
 video.playbackRate = 0.8; // Velocidade inicial
 
 function mudarVelocidade() {
-    let targetSpeed = checkbox.checked ? 8 : 0.8; // Velocidade desejada
+    let targetSpeed = checkbox.checked ? 16 : 0.8; // Velocidade desejada
     let currentSpeed = video.playbackRate; // Velocidade atual
     let increment = 0.8; // Incremento de velocidade
 
@@ -42,3 +44,51 @@ function mudarVelocidade() {
     }
 }
 
+// CONTROLE DOS TITULOS PESSOAIS
+
+
+
+const textos = [
+    'Desenvolvedor Full Stack',
+    'Navegador de Dados Interplanetário',
+    'Apaixonado por programação',
+    'Jedi',
+];
+const tituloPessoal = document.querySelector('.titulos-pessoais');
+
+let index = 0;
+let writingText = true; // Variável para indicar se está escrevendo ou apagando o texto
+
+function escreverTexto(texto, index = 0) {
+    if (index <= texto.length) {
+        tituloPessoal.innerHTML = texto.substring(0, index) + '<span class="barra">|</span>';
+        setTimeout(() => escreverTexto(texto, index + 1), 100);
+    } else {
+        writingText = false; // Marca que terminou de escrever o texto
+        setTimeout(apagarTexto, 1000); // Tempo de espera antes de apagar o texto
+    }
+}
+
+function apagarTexto() {
+    const textoAtual = tituloPessoal.textContent;
+    if (textoAtual.length > 0) {
+        tituloPessoal.textContent = textoAtual.slice(0, -1);
+        setTimeout(apagarTexto, 50);
+    } else {
+        writingText = true; // Marca que terminou de apagar o texto e está pronto para escrever novamente
+        index = (index + 1) % textos.length; // Avança para o próximo texto na matriz
+        setTimeout(() => escreverTexto(textos[index]), 500); // Tempo de espera antes de começar a escrever o próximo texto
+    }
+}
+
+setInterval(() => {
+    if (!writingText) {
+        const textoAtual = tituloPessoal.textContent;
+        if (textoAtual.charAt(textoAtual.length - 1) === '|') {
+            tituloPessoal.innerHTML = textoAtual.slice(0, -1);
+        } else {
+            tituloPessoal.innerHTML += '<span class="barra">|</span>';
+        }
+    }
+}, 500); // Adiciona e remove a barra intermitentemente
+escreverTexto(textos[index]);
